24a904e4a427b44e8798d444a9e8aa539356bc50
arkwave <ananthravikumar@berkeley.edu>
2017-08-09 18:04:32 +0400

    pep8 fixes

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,14 +2,14 @@
 # @Author: arkwave
 # @Date:   2017-08-09 17:01:19
 # @Last Modified by:   arkwave
-# @Last Modified time: 2017-08-09 17:21:25
+# @Last Modified time: 2017-08-09 18:04:15
 
-from scripts.util import combine_portfolios, create_straddle 
-from scripts.fetch_data import grab_data 
+from scripts.util import combine_portfolios, create_straddle
+from scripts.fetch_data import grab_data
 from scripts.portfolio import Portfolio
 
 
-import pandas as pd 
+import pandas as pd
 
 ######### variables ################
 start_date = '2017-05-01'
@@ -19,99 +19,97 @@ volids = ['QC  U7.U7', 'QC  Z7.Z7', 'CC  U7.U7', 'CC  Z7.Z7']
 ####################################
 
 vdf, pdf, edf = grab_data(pdts, start_date, end_date,
-	                      volids=volids, write=True)
+                          volids=volids, write=True)
 
 pdf = pdf[pdf.value_date == pd.to_datetime(start_date)]
 vdf = vdf[vdf.value_date == pd.to_datetime(start_date)]
 
 
 def test_create_underlying():
-	pass 
+    pass
 
 
 def test_create_vanilla_option():
-	pass 
+    pass
 
 
 def test_create_straddle():
-	pass 
+    pass
 
 
 def test_create_strangle():
-	pass 
+    pass
 
 
 def test_create_butterfly():
-	pass 
+    pass
+
 
 def test_create_spread():
-	pass 
+    pass
+
 
 def test_create_skew():
-	pass 
+    pass
 
 
 def test_create_composites():
-	pass 
+    pass
 
 
 def test_merge_dicts():
-	pass 
+    pass
 
 
 def test_merge_lists():
-	pass 
-
+    pass
 
 
 def test_combine_portfolios():
 
-	cc1, cc2 = create_straddle('CC  U7.U7', vdf, pdf, pd.to_datetime(
-	    start_date), False, 'atm', greek='vega', greekval=20000)
+    cc1, cc2 = create_straddle('CC  U7.U7', vdf, pdf, pd.to_datetime(
+        start_date), False, 'atm', greek='vega', greekval=20000)
 
-	qc1, qc2 = create_straddle('QC  U7.U7', vdf, pdf, pd.to_datetime(
-	    start_date), True, 'atm', greek='vega', greekval=20000)
+    qc1, qc2 = create_straddle('QC  U7.U7', vdf, pdf, pd.to_datetime(
+        start_date), True, 'atm', greek='vega', greekval=20000)
 
-	pf1 = Portfolio()
-	pf1.add_security([qc1, qc2], 'OTC')
+    pf1 = Portfolio()
+    pf1.add_security([qc1, qc2], 'OTC')
 
-	pf2 = Portfolio() 
-	pf2.add_security([cc1, cc2], 'OTC')
-	
-	pf3 = combine_portfolios([pf1, pf2])
+    pf2 = Portfolio()
+    pf2.add_security([cc1, cc2], 'OTC')
 
-	# OTC check 
-	otc = pf1.OTC.copy()
-	otc.update(pf2.OTC.copy())
-	for pdt in otc:
-		for mth in otc[pdt]:
-			assert pf3.OTC[pdt][mth] == otc[pdt][mth]
+    pf3 = combine_portfolios([pf1, pf2])
 
-	# hedge check 
-	hed = pf1.hedges.copy()
-	hed.update(pf2.hedges.copy())
-	for pdt in hed:
-		for mth in hed[pdt]:
-			assert pf3.hedges[pdt][mth] == hed[pdt][mth]
+    # OTC check
+    otc = pf1.OTC.copy()
+    otc.update(pf2.OTC.copy())
+    for pdt in otc:
+        for mth in otc[pdt]:
+            assert pf3.OTC[pdt][mth] == otc[pdt][mth]
 
-	# net check 
-	net_tst = pf1.net_greeks.copy() 
-	net_tst.update(pf2.net_greeks)
-	for pdt in net_tst:
-		for mth in net_tst[pdt]:
-			assert net_tst[pdt][mth] == pf3.net_greeks[pdt][mth]
+    # hedge check
+    hed = pf1.hedges.copy()
+    hed.update(pf2.hedges.copy())
+    for pdt in hed:
+        for mth in hed[pdt]:
+            assert pf3.hedges[pdt][mth] == hed[pdt][mth]
 
-	# lists check 
-	otcops = pf1.OTC_options.copy()
-	otcops.extend(pf2.OTC_options)
-	assert otcops == pf3.OTC_options 
+    # net check
+    net_tst = pf1.net_greeks.copy()
+    net_tst.update(pf2.net_greeks)
+    for pdt in net_tst:
+        for mth in net_tst[pdt]:
+            assert net_tst[pdt][mth] == pf3.net_greeks[pdt][mth]
 
-	hops = pf1.hedge_options.copy() 
-	hops.extend(pf2.hedge_options)
-	assert hops == pf3.hedge_options 
+    # lists check
+    otcops = pf1.OTC_options.copy()
+    otcops.extend(pf2.OTC_options)
+    assert otcops == pf3.OTC_options
 
-	assert pf3.families == [pf1, pf2]
-	assert isinstance(pf3, Portfolio)
+    hops = pf1.hedge_options.copy()
+    hops.extend(pf2.hedge_options)
+    assert hops == pf3.hedge_options
 
-
-
+    assert pf3.families == [pf1, pf2]
+    assert isinstance(pf3, Portfolio)
